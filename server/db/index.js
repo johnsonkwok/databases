0d0e2d828d835bbad4e0038f80a724c7c0abc89b
johnsonkwok <johnsonkwok1@gmail.com>
2018-12-05 21:53:17 -0800

    Set up client folder

--- server/db/index.js
+++ server/db/index.js
@@ -2,14 +2,9 @@ var mysql = require('mysql');
 
 const dbConnection = mysql.createConnection({
   user: 'root',
-  password: NO,
   database: 'chat'
 });
 dbConnection.connect();
-
-    var tablename = "messages"; // TODO: fill this out
-
-dbConnection.query('truncate ' + tablename, done);
 
 module.exports = dbConnection;
 // Create a database connection and export it from this file.
--- server/schema.sql
+++ server/schema.sql
@@ -32,6 +32,116 @@ CREATE TABLE rooms (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+ -- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Messages'
+-- 
+-- ---
+
+
+
+
+-- DROP TABLE IF EXISTS `messages`;
+		
+-- CREATE TABLE `messages` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `content` MEDIUMTEXT(300) NULL DEFAULT NULL,
+--   `userId` INTEGER NULL DEFAULT NULL,
+--   `roomId` INTEGER NULL DEFAULT NULL,
+--   `createdAt` TIMESTAMP NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- -- ---
+-- -- Table 'Users'
+-- -- 
+-- -- ---
+
+-- DROP TABLE IF EXISTS `Users`;
+		
+-- CREATE TABLE `Users` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `username` MEDIUMTEXT(25) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- -- ---
+-- -- Table 'Rooms'
+-- -- 
+-- -- ---
+
+-- DROP TABLE IF EXISTS `Rooms`;
+		
+-- CREATE TABLE `Rooms` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `roomname` MEDIUMTEXT(25) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- -- ---
+-- -- Table 'Rooms/Messages'
+-- -- 
+-- -- ---
+
+-- DROP TABLE IF EXISTS `Rooms/Messages`;
+		
+-- CREATE TABLE `Rooms/Messages` (
+--   `messageId` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `roomId` INTEGER NULL DEFAULT NULL
+-- );
+
+-- -- ---
+-- -- Table 'Users/Messages'
+-- -- 
+-- -- ---
+
+-- DROP TABLE IF EXISTS `Users/Messages`;
+		
+-- CREATE TABLE `Users/Messages` (
+--   `messageId` INTEGER NULL DEFAULT NULL,
+--   `userId` INTEGER NULL DEFAULT NULL
+-- );
+
+-- -- ---
+-- -- Foreign Keys 
+-- -- ---
+
+-- ALTER TABLE `Rooms/Messages` ADD FOREIGN KEY (messageId) REFERENCES `Messages` (`id`);
+-- ALTER TABLE `Rooms/Messages` ADD FOREIGN KEY (roomId) REFERENCES `Rooms` (`id`);
+-- ALTER TABLE `Users/Messages` ADD FOREIGN KEY (messageId) REFERENCES `Messages` (`id`);
+-- ALTER TABLE `Users/Messages` ADD FOREIGN KEY (userId) REFERENCES `Users` (`id`);
+
+-- -- ---
+-- -- Table Properties
+-- -- ---
+
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms/Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users/Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- -- ---
+-- -- Test Data
+-- -- ---
+
+-- INSERT INTO `Messages` (`id`,`content`,`userId`,`roomId`,`createdAt`) VALUES
+-- ('','','','','');
+-- INSERT INTO `Users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `Rooms` (`id`,`roomname`) VALUES
+-- ('','');
+-- INSERT INTO `Rooms/Messages` (`messageId`,`roomId`) VALUES
+-- ('','');
+-- INSERT INTO `Users/Messages` (`messageId`,`userId`) VALUES
+-- ('','');
+
 
 
 
