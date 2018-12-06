1c5224c20d75c4703cc12d9df92a79f2ffedceaf
johnsonkwok <johnsonkwok1@gmail.com>
2018-12-05 17:31:44 -0800

    Implement tables

--- server/schema.sql
+++ server/schema.sql
@@ -3,8 +3,25 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER IDENTITY(1,1) PRIMARY KEY,
+  content TEXT NOT NULL,
+  username INTEGER,
+  roomname INTEGER,
+  createdAt TEXT NOT NULL,
+  FOREIGN KEY(username) REFERENCES users(id),
+  FOREIGN KEY(roomname) REFERENCES rooms(id)
 );
+
+CREATE TABLE users (
+  id INTEGER IDENTITY(1,1) PRIMARY KEY,
+  username TEXT NOT NULL,
+);
+
+CREATE TABLE rooms (
+  id INTEGER IDENTITY(1,1) PRIMARY KEY,
+  roomname TEXT NOT NULL,
+);
+
 
 /* Create other tables and define schemas for them here! */
 
